971f7b0ddd45164d22ab6dc489c3501c542f309d
Gabriel Pereira <berkpereira@gmail.com>
2024-06-04 19:24:03 +0100

    implemented SolverOutput class (self-descriptive name)

---
+++ solvers/utils.py
@@ -0,0 +1,7 @@
+class SolverOutput():
+    def __init__(self, final_x, final_k, f_vals, grad_vals=None, grad_norms=None):
+        self.final_x = final_x
+        self.final_k = final_k
+        self.f_vals = f_vals
+        self.grad_vals = grad_vals
+        self.grad_norms = grad_norms

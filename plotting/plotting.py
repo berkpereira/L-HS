0268d410f250fba1dc3bd25a534ad4631e2857a6
Gabriel Pereira <berkpereira@gmail.com>
2024-06-06 16:30:53 +0100

    allows for comparison of multiple solver runs

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -1,16 +1,25 @@
 import matplotlib.pyplot as plt
+from solvers.utils import SolverOutput
 
-def plot_loss_vs_iteration(f_vals):
+def plot_loss_vs_iteration(solver_outputs, labels=None):
     """
-    Plot the loss (function values) vs iteration count.
+    Plot the loss (function values) vs iteration count for multiple solvers.
 
-    :param f_vals: Array of function values over iterations.
+    :param solver_outputs: List of SolverOutput instances.
+    :param labels: List of labels for each solver output.
     """
     plt.figure(figsize=(10, 6))
-    plt.plot(f_vals, linestyle='-', color='b')
+    
+    if labels is None:
+        labels = [f"Solver {i+1}" for i in range(len(solver_outputs))]
+
+    for solver_output, label in zip(solver_outputs, labels):
+        plt.plot(solver_output.f_vals, linestyle='-', label=label)
+    
     plt.yscale('log')  # Set the vertical axis to log scale
     plt.xlabel('Iteration')
     plt.ylabel('Function value (log scale)')
-    plt.title('Loss vs Iteration')
+    plt.title('Loss vs Iteration Comparison')
+    plt.legend()
     plt.grid(True, which="both", ls="-")
     plt.show()

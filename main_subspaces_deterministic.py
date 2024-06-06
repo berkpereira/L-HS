86acc77c883513708efae5beece06e48543d2223
Gabriel Pereira <berkpereira@gmail.com>
2024-06-06 17:09:03 +0100

    makes convenience changes

--- main_subspaces_deterministic.py
+++ main_subspaces_deterministic.py
@@ -10,31 +10,30 @@ test_problems_list = ['rosenbrock', # 0
                     'well_conditioned_convex_quadratic', # 2
                     'ill_conditioned_convex_quadratic'] # 3
 
-subspace_methods_list = ['grads',
-                         'iterates_grads',
-                         'iterates_grads_diagnewtons']
+subspace_methods_list = ['grads', # 0
+                         'iterates_grads', # 1
+                         'iterates_grads_diagnewtons'] # 2
 
-PROBLEM_NAME = test_problems_list[1]
+PROBLEM_NAME = test_problems_list[0]
 INPUT_DIM = 24 # NOTE: depending on the problem, this may have no effect
-SUBSPACE_DIM = 12
+
 SUBSPACE_METHOD = subspace_methods_list[1]
-REG_LAMBDA = 0.01
+REG_LAMBDA = 0.001
+
+TOL = 1e-10
+MAX_ITER = 10000
 
 ITER_PRINT_GAP = 20
-
-TOL = 1e-5
-MAX_ITER = 10000
 
 # Instantiate problem
 x0, obj = problems.test_problems.select_problem(problem_name=PROBLEM_NAME, input_dim=INPUT_DIM)
 
-# Initialise optimiser
-optimiser = solvers.subspaces_deterministic.common_directions.CommonDirections(obj=obj, subspace_update_method=SUBSPACE_METHOD,subspace_dim=SUBSPACE_DIM, reg_lambda=REG_LAMBDA, alpha=0.01, t_init=1, tol=TOL, max_iter=MAX_ITER, iter_print_gap=ITER_PRINT_GAP, verbose=True)
-
-# Perform optimisation
+# Run solver(s)
 output_list = []
-for input_dim in range(6, 25, 6):
+for SUBSPACE_DIM in range(6, 25, 6):
+    optimiser = solvers.subspaces_deterministic.common_directions.CommonDirections(obj=obj, subspace_update_method=SUBSPACE_METHOD,subspace_dim=SUBSPACE_DIM, reg_lambda=REG_LAMBDA, alpha=0.01, t_init=1, tol=TOL, max_iter=MAX_ITER, iter_print_gap=ITER_PRINT_GAP, verbose=True)    
     output = optimiser.optimise(x0)
     output_list.append(output)
 
+# Plot
 plotting.plotting.plot_loss_vs_iteration(solver_outputs=output_list)

11fdfad74dbaed1ea2f65697f4d5a39f29d565ed
Gabriel Pereira <berkpereira@gmail.com>
2024-06-07 17:38:55 +0100

    fixes arguments errors

--- solvers/classical/linesearch_general.py
+++ solvers/classical/linesearch_general.py
@@ -9,17 +9,18 @@ import autograd.numpy as np
 from ..utils import SolverOutput
 
 class LinesearchGeneral:
-    def __init__(self, obj, iterate_info_func: callable, direction_func: callable, step_func: callable, stop_crit_func: callable, max_iter: int, iter_print_period: int=20, **kwargs):
+    def __init__(self, obj, deriv_info_func: callable, direction_func: callable, step_func: callable, stop_crit_func: callable, max_iter: int, iter_print_period: int=20, verbose=False, **kwargs):
         self.obj = obj # Objective class instance
         self.func = self.obj.func
         self.grad_func = grad(self.func)
-        self.iterate_info_func = iterate_info_func
+        self.deriv_info_func = deriv_info_func # May return gradient, hessian, etc.
         self.direction_func = direction_func # Computes search direction
         self.step_func = step_func # Computes step size
         self.stop_crit_func = stop_crit_func # Callable returning Boolean for termination when its output at an iteration is True
         
         self.max_iter = max_iter
         self.iter_print_period = iter_print_period
+        self.verbose = verbose
 
         # kwargs can include a variety of other variables/callables to assist in the optimisation process (stopping criteria/backtracking, etc.)
         self.kwargs = kwargs
@@ -28,9 +29,11 @@ class LinesearchGeneral:
         x = x0
         k = 0
         f_x = self.func(x)
-        grad_f_x = self.grad_func
+
+        # deriv_info should be a small list, with elements containing derivatives at x of increasing order
+        deriv_info = self.deriv_info_func(x)
         
-        stop_cond = self.stop_crit_func(self.obj, self.max_iter, k, x, f_x, grad_f_x, self.kwargs)
+        stop_cond = self.stop_crit_func(self.obj, self.max_iter, k, x, f_x, deriv_info, self.kwargs)
 
         f_vals = [f_x]
 
@@ -38,17 +41,18 @@ class LinesearchGeneral:
             k += 1
             
             # Compute search direction and step
-            search_dir = self.direction_func(self.obj, x, grad_f_x, self.kwargs)
-            step = self.step_func(self.obj, x, grad_f_x, self.kwargs) # e.g., using backtracking armijo
+            search_dir = self.direction_func(self.obj, x, deriv_info, self.kwargs)
+            step = self.step_func(self.obj, x, search_dir, deriv_info, self.kwargs) # e.g., using backtracking Armijo
 
             # Update relevant quantities
             x = x + step * search_dir
+            
             f_x = self.obj.func(x)
-            grad_f_x = self.grad_func(x)
+            deriv_info = self.deriv_info_func(x)
 
             f_vals.append(f_x)
             
-            stop_cond = self.stop_crit_func(self.obj, self.max_iter, k, x, grad_f_x, self.kwargs)
+            stop_cond = self.stop_crit_func(self.obj, self.max_iter, k, x, f_x, deriv_info, self.kwargs)
         
         f_vals = np.array(f_vals) # convert into np array
         

efa3cc3b90147c96010b97175bf8c087336dd6de
Gabriel Pereira <berkpereira@gmail.com>
2024-06-07 15:16:11 +0100

    introduces main script for random subspace methods

---
+++ main_subspaces_random.py
@@ -0,0 +1,11 @@
+import problems.test_problems
+import solvers
+import plotting
+import os
+os.system('clear')
+
+# For reference:
+test_problems_list = ['rosenbrock', # 0
+                    'powell', # 1
+                    'well_conditioned_convex_quadratic', # 2
+                    'ill_conditioned_convex_quadratic'] # 3

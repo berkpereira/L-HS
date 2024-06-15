13392913c127c9b01197c1a68f7d01f5b70c0719
Gabriel Pereira <berkpereira@gmail.com>
2024-06-15 22:46:01 +0100

    adjusts to newer optimiser class form

--- main_subspaces_deterministic.py
+++ main_subspaces_deterministic.py
@@ -17,11 +17,11 @@ subspace_methods_list = ['grads', # 0
 PROBLEM_NAME = test_problems_list[0]
 INPUT_DIM = 24 # NOTE: depending on the problem, this may have no effect
 
-SUBSPACE_METHOD = subspace_methods_list[1]
+SUBSPACE_METHOD = subspace_methods_list[0]
 REG_LAMBDA = 0.001
 
 TOL = 1e-10
-MAX_ITER = 10000
+MAX_ITER = 100
 
 ITER_PRINT_GAP = 20
 
@@ -31,7 +31,8 @@ x0, obj = problems.test_problems.select_problem(problem_name=PROBLEM_NAME, input
 # Run solver(s)
 output_list = []
 for SUBSPACE_DIM in range(6, 25, 6):
-    optimiser = solvers.subspaces_deterministic.common_directions.CommonDirections(obj=obj, subspace_update_method=SUBSPACE_METHOD,subspace_dim=SUBSPACE_DIM, reg_lambda=REG_LAMBDA, alpha=0.01, t_init=1, tol=TOL, max_iter=MAX_ITER, iter_print_gap=ITER_PRINT_GAP, verbose=True)    
+    print(f'INPUT DIMENSION: {INPUT_DIM}. SOLVER WITH SUBSPACE DIMENSION: {SUBSPACE_DIM}')
+    optimiser = solvers.subspaces_deterministic.common_directions.CommonDirections(obj=obj, subspace_update_method=SUBSPACE_METHOD,subspace_dim=SUBSPACE_DIM, reg_lambda=REG_LAMBDA, alpha=0.01, t_init=1, tol=TOL, max_iter=MAX_ITER, iter_print_gap=ITER_PRINT_GAP, verbose=False)    
     output = optimiser.optimise(x0)
     output_list.append(output)
 

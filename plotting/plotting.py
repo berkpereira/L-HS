0b502e93e7e0e8fcc1a852da3c8e4dfccd286ef5
Gabriel Pereira <berkpereira@gmail.com>
2024-06-15 22:45:37 +0100

    upgrades plot labels

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -11,7 +11,7 @@ def plot_loss_vs_iteration(solver_outputs, labels=None):
     plt.figure(figsize=(10, 6))
     
     if labels is None:
-        labels = [f"Solver {i+1}" for i in range(len(solver_outputs))]
+        labels = [f"Subspace dim = {solver_outputs[i].solver.subspace_dim}" for i in range(len(solver_outputs))]
 
     for solver_output, label in zip(solver_outputs, labels):
         plt.plot(solver_output.f_vals, linestyle='-', label=label)

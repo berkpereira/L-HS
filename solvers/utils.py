d7fcc4a4f8d4d642d4261d13dac930ddee6ba255
Gabriel Pereira <berkpereira@gmail.com>
2024-06-15 22:46:12 +0100

    adds solver attribute to SolverOutput class

--- solvers/utils.py
+++ solvers/utils.py
@@ -1,7 +1,8 @@
 class SolverOutput():
-    def __init__(self, final_x, final_k, f_vals, grad_vals=None, grad_norms=None):
+    def __init__(self, solver, final_x, final_k, f_vals, grad_vals=None, grad_norms=None):
         self.final_x = final_x
         self.final_k = final_k
         self.f_vals = f_vals
         self.grad_vals = grad_vals
         self.grad_norms = grad_norms
+        self.solver = solver

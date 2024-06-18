f3b6e69a38349dcbb4e5bf892194020541ca9f78
Gabriel Pereira <berkpereira@gmail.com>
2024-06-18 16:32:42 +0100

    moves things

--- solvers/full_space/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import GN
-from . import linesearch_bArmijo
-# may need to add more as we go along
--- solvers/subspaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import common_directions
-from . import projected_common_directions
-from . import cartis2022algorithm3

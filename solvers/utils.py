afa8c8e172ee009e7b3c5cd0ab8a7d2a2824c2c8
Gabriel Pereira <berkpereira@gmail.com>
2024-06-24 11:37:54 +0100

    implements haar matrix generation. moves scaled gaussian generation into utils

--- solvers/utils.py
+++ solvers/utils.py
@@ -1,4 +1,5 @@
-import scipy.optimize
+import scipy, scipy.optimize, scipy.linalg
+import autograd.numpy as np
 
 class SolverOutput():
     def __init__(self, solver, final_x, final_k, f_vals, update_norms=None, **kwargs):
@@ -21,3 +22,22 @@ def strong_wolfe_linesearch(func, grad_func, x, direction, c1, c2, max_iter):
     return scipy.optimize.line_search(func, grad_func, x,
                                         direction, c1=c1, c2=c2,
                                         maxiter=max_iter)
+
+
+# TALL(!) scaled Gaussian sketching matrix!
+# Subspace dimension taken to be the number of columns, n
+def scaled_gaussian(m, n):
+    return np.random.normal(scale=np.sqrt(1 / n), size=(m, n))
+
+# NOTE: The function below is adapted from Mezzadri2007, Sec. 5
+# (arXiv:math-ph/0609050)
+# Generates a random orthonormal matrix with dimensions (m, n)
+def haar(m, n):
+    z = np.random.randn(m,m) / np.sqrt(2.0)
+    q,r = np.linalg.qr(z)
+    d = np.diagonal(r)
+    ph = d / np.absolute(d)
+    q = np.multiply(q, ph, q)
+    
+    # Select first n rows to return
+    return q[:, :n]

a081ef315bfeced7e5747c742439b25fe3ea1070
Gabriel Pereira <berkpereira@gmail.com>
2024-07-02 00:22:26 +0100

    improves

--- main_projected_common_directions.py
+++ main_projected_common_directions.py
@@ -1,10 +1,16 @@
 import problems.test_problems
+import autograd.numpy as np
 from solvers.projected_common_directions import ProjectedCommonDirections, ProjectedCommonDirectionsConfig
 import plotting
 import matplotlib.pyplot as plt
 import os
 
+np.random.seed(42)
 os.system('clear')
+
+################################################################################
+################################################################################
+################################################################################
 
 # For reference:
 test_problems_list = ['rosenbrock',                      # 0
@@ -12,80 +18,92 @@ test_problems_list = ['rosenbrock',                      # 0
                     'well_conditioned_convex_quadratic', # 2
                     'ill_conditioned_convex_quadratic']  # 3
 
-subspace_methods_list = ['grads',                        # 0
-                         'iterates_grads',               # 1
-                         'iterates_grads_diagnewtons']   # 2
-
 # SELECT PROBLEM
 PROBLEM_NAME = test_problems_list[0]
-INPUT_DIM = 40
+INPUT_DIM = 20
 x0, obj = problems.test_problems.select_problem(problem_name=PROBLEM_NAME, input_dim=INPUT_DIM)
 
 # SOLVER CONFIG
-SUBSPACE_METHOD = subspace_methods_list[1]
-
-SUBSPACE_DIM = 12
 
 REG_LAMBDA = 0.01
 USE_HESS = True
 
-RANDOM_PROJ = False
+INNER_USE_FULL_GRAD = True
+REPROJECT_GRAD = False       # NOTE: makes no difference in the randomised proj variant
 
-# NOTE: makes no difference in the randomised case
-REPROJECT_GRAD = True
+RANDOM_PROJ = True
 
 ENSEMBLE = 'haar'
-
 ALPHA = 0.01
 T_INIT = 1
 TAU = 0.5
 
-MAX_ITER = 10_000
+MAX_ITER = np.inf
+DERIV_BUDGET = 3_000
+
 TOL = 1e-3
-ITER_PRINT_GAP = 50
+ITER_PRINT_GAP = 20
 
 # Run solver(s)
 output_list = []
-for append_rand_dirs in [0, 2, 4, 6, 8]:
-    SOLVER_CONFIG = ProjectedCommonDirectionsConfig(obj=obj,
-                                            subspace_update_method=SUBSPACE_METHOD,
-                                            subspace_dim=SUBSPACE_DIM,
-                                            append_rand_dirs=append_rand_dirs,
-                                            reg_lambda=REG_LAMBDA,
-                                            use_hess=USE_HESS,
-                                            random_proj=RANDOM_PROJ,
-                                            reproject_grad=REPROJECT_GRAD,
-                                            ensemble=ENSEMBLE,
-                                            alpha=ALPHA,
-                                            t_init=T_INIT,
-                                            tau=TAU,
-                                            tol=TOL,
-                                            max_iter=MAX_ITER,
-                                            iter_print_gap=ITER_PRINT_GAP,
-                                            verbose=True)
 
-    optimiser = ProjectedCommonDirections(config=SOLVER_CONFIG)
-    output = optimiser.optimise(x0)
-    output_list.append(output)
+SUBSPACE_NO_LIST = [(INPUT_DIM, 0, 0),
+                    (1, 1, 2),
+                    (2, 2, 2),
+                    (3, 3, 2)]
+
+for SUBSPACE_NO_GRADS, SUBSPACE_NO_UPDATES, SUBSPACE_NO_RANDOM in SUBSPACE_NO_LIST:
+    SUBSPACE_DIM = SUBSPACE_NO_GRADS + SUBSPACE_NO_UPDATES + SUBSPACE_NO_RANDOM
+    RANDOM_PROJ_DIM = SUBSPACE_DIM # Does not need to be...
+    for _ in range(2): # how many runs per solver?
+        SOLVER_CONFIG = ProjectedCommonDirectionsConfig(obj=obj,
+                                                        subspace_no_grads=SUBSPACE_NO_GRADS,
+                                                        subspace_no_updates=SUBSPACE_NO_UPDATES,
+                                                        subspace_no_random=SUBSPACE_NO_RANDOM,
+                                                        inner_use_full_grad=INNER_USE_FULL_GRAD,
+                                                        reg_lambda=REG_LAMBDA,
+                                                        use_hess=USE_HESS,
+                                                        random_proj=RANDOM_PROJ,
+                                                        random_proj_dim=RANDOM_PROJ_DIM,
+                                                        reproject_grad=REPROJECT_GRAD,
+                                                        ensemble=ENSEMBLE,
+                                                        alpha=ALPHA,
+                                                        t_init=T_INIT,
+                                                        tau=TAU,
+                                                        tol=TOL,
+                                                        max_iter=MAX_ITER,
+                                                        deriv_budget=DERIV_BUDGET,
+                                                        iter_print_gap=ITER_PRINT_GAP,
+                                                        verbose=True)
+
+        optimiser = ProjectedCommonDirections(config=SOLVER_CONFIG)
+        output = optimiser.optimise(x0)
+        output_list.append(output)
 
 # PLOTTING
-plotting.plotting.plot_loss_vs_iteration(solver_outputs=output_list)
-plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
-                                           attr_names=['update_norms'],
-                                           log_plot=True)
+plotting.plotting.plot_loss_vs_iteration(solver_outputs=output_list,
+                                         deriv_evals_axis=True)
 
-plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
-                                           attr_names=['direction_norms'],
-                                           log_plot=True)
+# plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
+#                                            attr_names=['update_norms'],
+#                                            log_plot=True)
 
-plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
-                                           attr_names=['full_grad_norms',
-                                                       'proj_grad_norms'],
-                                           log_plot=True)
+# plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
+#                                            attr_names=['direction_norms'],
+#                                            log_plot=True)
 
-plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
-                                           attr_names=['angles_to_full_grad'],
-                                           log_plot=False)
+# plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
+#                                            attr_names=['P_norms'],
+#                                            log_plot=True)
+
+# plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
+#                                            attr_names=['full_grad_norms',
+#                                                        'proj_grad_norms'],
+#                                            log_plot=True)
+
+# plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
+#                                            attr_names=['angles_to_full_grad'],
+#                                            log_plot=False)
 
 # plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
 #                                            attr_names=['cond_nos'],

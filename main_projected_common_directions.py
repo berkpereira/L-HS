50fd24e6bd024e25706559638f9b2e9c6f180444
Gabriel Pereira <berkpereira@gmail.com>
2024-07-28 23:06:20 +0100

    adds CUTEst problems with most flexility

--- main_projected_common_directions.py
+++ main_projected_common_directions.py
@@ -57,7 +57,7 @@ def run_solvers(problem_tup, solvers_list, no_runs, result_attrs,
     
     return results_dict
 
-def plot_run_solvers(output_dict):
+def plot_run_solvers(output_dict, normalise_loss):
     # Extract all SolverOutput objects from a results dictionary.
     output_list = [solver_output for _, solver_outputs in output_dict['raw_results'] for solver_output in solver_outputs]
     
@@ -66,11 +66,13 @@ def plot_run_solvers(output_dict):
                                              deriv_evals_axis=True,
                                              normalise_deriv_evals_vs_dimension=True,
                                              normalise_P_k_dirs_vs_dimension=True,
-                                             normalise_S_k_dirs_vs_dimension=True)
+                                             normalise_S_k_dirs_vs_dimension=True,
+                                             normalise_loss_data=normalise_loss)
     plotting.plotting.plot_loss_vs_iteration(solver_outputs=output_list,
                                              deriv_evals_axis=False,
                                              normalise_P_k_dirs_vs_dimension=False,
-                                             normalise_S_k_dirs_vs_dimension=False)
+                                             normalise_S_k_dirs_vs_dimension=False,
+                                             normalise_loss_data=normalise_loss)
     # plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
     #                                            attr_names=['update_norms'], log_plot=True)
     # plotting.plotting.plot_scalar_vs_iteration(solver_outputs=output_list,
@@ -108,21 +110,29 @@ def main():
     set_seed(42)
 
     # Choose problem
-    test_problems_list = ['rosenbrock_single',                 # 0
-                          'rosenbrock_multiple',               # 1
-                          'powell',                            # 2
-                          'well_conditioned_convex_quadratic', # 3
-                          'ill_conditioned_convex_quadratic',  # 4
-                          'NONDIA',                            # 5
-                          'GENHUMPS',                          # 6
-                          'ARGLINA']                           # 7
-    problem_name = test_problems_list[0]
+    test_problems_list = ['rosenbrock_single',                 # 0, n even
+                          'rosenbrock_multiple',               # 1, n even
+                          'powell',                            # 2, n multiple of 4
+                          'well_conditioned_convex_quadratic', # 3, any n
+                          'ill_conditioned_convex_quadratic',  # 4, any n
+                          'POWELLSG',                          # 5,  n in {4, 8, 16, 20, 36, 40, 60, 80, 100, 500}
+                          'POWER',                             # 6,  n in {10, 20, 30, 50, 75, 100, 500}
+                          'NONDIA',                            # 7,  n in {10, 20, 30, 50, 90, 100, 500}
+                          'NCB20B',                            # 8,  n in {21, 22, 50, 100, 180, 500}
+                          'OSCIGRAD',                          # 9,  n in {2, 5, 10, 15, 25, 100}
+                          'TRIDIA',                            # 10, n in {10, 20, 30, 50, 100, 500}
+                          'OSCIPATH',                          # 11, n in {2, 5, 10, 25, 100, 500}
+                          'YATP2LS',                           # 12, n in {2, 10, 50, 100, 200, 350}
+                          'PENALTY2']                          # 13, n in {4, 10, 50, 100, 200, 500}
+    problem_name = test_problems_list[8]
     input_dim = 100
     problem_tup = get_problem(problem_name, input_dim)
+    NORMALISE_LOSS = False
     SAVE_RESULTS = False
 
     passable_name = 'passable2'
-    configs_list = [combine_configs(problem_name, input_dim, 'solver0', passable_name),
+    configs_list = [combine_configs(problem_name, input_dim, 'full_newton', passable_name),
+                    combine_configs(problem_name, input_dim, 'solver0', passable_name),
                     combine_configs(problem_name, input_dim, 'solver3', passable_name),
                     combine_configs(problem_name, input_dim, 'solver4', passable_name)]
     solvers_list = [ProjectedCommonDirections(config) for config in configs_list]
@@ -133,12 +143,12 @@ def main():
                                result_attrs=results_attrs, save_results=SAVE_RESULTS)
 
     # Plot
-    plotting.plotting.plot_solver_averages(results_dict, ['final_f_val'])
-    plotting.plotting.plot_run_histograms(results_dict['raw_results'],
-                                          attr_names=results_attrs)
+    # plotting.plotting.plot_solver_averages(results_dict, ['final_f_val'])
+    # plotting.plotting.plot_run_histograms(results_dict['raw_results'],
+    #                                       attr_names=results_attrs)
 
     # (detailed plots, each individual run represented)
-    plot_run_solvers(results_dict)
+    plot_run_solvers(results_dict, NORMALISE_LOSS)
 
     plt.show()
 

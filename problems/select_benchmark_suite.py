754ca3d9a99a195fad66837edb4e2d0e811156f8
Gabriel Pereira <berkpereira@gmail.com>
2024-08-06 22:34:30 +0100

    now selects suitable small-benchmark problems randomly

--- problems/select_benchmark_suite.py
+++ problems/select_benchmark_suite.py
@@ -12,31 +12,53 @@ import os
 import pycutest
 import json
 import solvers.utils
+import random
 
 def load_json_to_dict(file_path):
     with open(file_path, 'r') as file:
         data_dict = json.load(file)
     return data_dict
 
-def main():
-    os.system('clear')
+def generate_chosen_dict():
     results_dict = load_json_to_dict('results/best_known_results.json')
     no_suitable = 0
     suitable_name_dim_dict = {}
     for extended_name in results_dict:
         name, input_dim = solvers.utils.problem_name_dim_tuple_from_json_name(extended_name)
         best_sol = results_dict[extended_name]
-        if 50 <= input_dim <= 200 and best_sol < 1e4:
-            print(f'Name: {name}, dim: {input_dim}')
-            print(f'Best sol.: {results_dict[extended_name]}')
+        if 90 <= input_dim <= 200 and best_sol < 1e4:
+            # print(f'Name: {name}, dim: {input_dim}')
+            # print(f'Best sol.: {results_dict[extended_name]}')
             if name in suitable_name_dim_dict:
                 suitable_name_dim_dict[name].append(input_dim)
             else:
                 suitable_name_dim_dict.update({name: [input_dim]})
             no_suitable += 1
-    print(f'No. suitable problems: {no_suitable}')
+    return suitable_name_dim_dict, no_suitable
 
+def select_random_combinations(input_dict, num_combinations):
+    # Select 5 keys uniformly at random from the dictionary
+    selected_keys = random.sample(list(input_dict.keys()), num_combinations)
+    
+    # For each selected key, select one of the elements in the corresponding list value uniformly at random
+    combinations = []
+    for key in selected_keys:
+        value = random.choice(input_dict[key])
+        combinations.append(f"{key}_n{value}")
+    
+    return combinations
+
+def main():
+    os.system('clear')
+    suitable_name_dim_dict, no_suitable = generate_chosen_dict()
+    print(f'No. suitable problems: {no_suitable}')
     print(suitable_name_dim_dict)
+
+    print()
+    print()
+
+    chosen_problems = select_random_combinations(suitable_name_dim_dict, 20)
+    print(chosen_problems)
     
 
 if __name__ == '__main__':

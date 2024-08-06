5fd7c8545548c8dde4c2b60f59f4dfdb426c88d4
Gabriel Pereira <berkpereira@gmail.com>
2024-08-06 22:49:35 +0100

    selects and writes 20 CUTEst problems for small data profiles throughout thesis

--- problems/select_benchmark_suite.py
+++ problems/select_benchmark_suite.py
@@ -14,10 +14,16 @@ import json
 import solvers.utils
 import random
 
+random.seed(4)
+
 def load_json_to_dict(file_path):
     with open(file_path, 'r') as file:
         data_dict = json.load(file)
     return data_dict
+
+def write_json_file(json_file_name: str, string_list: list):
+    with open(json_file_name, 'w') as json_file:
+        json.dump(string_list, json_file)
 
 def generate_chosen_dict():
     results_dict = load_json_to_dict('results/best_known_results.json')
@@ -26,7 +32,7 @@ def generate_chosen_dict():
     for extended_name in results_dict:
         name, input_dim = solvers.utils.problem_name_dim_tuple_from_json_name(extended_name)
         best_sol = results_dict[extended_name]
-        if 90 <= input_dim <= 200 and best_sol < 1e4:
+        if 100 <= input_dim <= 200 and best_sol < 1e4:
             # print(f'Name: {name}, dim: {input_dim}')
             # print(f'Best sol.: {results_dict[extended_name]}')
             if name in suitable_name_dim_dict:
@@ -51,14 +57,17 @@ def select_random_combinations(input_dict, num_combinations):
 def main():
     os.system('clear')
     suitable_name_dim_dict, no_suitable = generate_chosen_dict()
-    print(f'No. suitable problems: {no_suitable}')
-    print(suitable_name_dim_dict)
+    print(f'NO. SUITABLE PROBLEMS AS FILTERED: {no_suitable}')
+    # print(suitable_name_dim_dict)
 
-    print()
-    print()
+    WRITE_TO_FILE = True
 
     chosen_problems = select_random_combinations(suitable_name_dim_dict, 20)
     print(chosen_problems)
+
+    if WRITE_TO_FILE:
+        JSON_FILE_NAME = 'problems/small_profile_problems.json'
+        write_json_file(JSON_FILE_NAME, chosen_problems)
     
 
 if __name__ == '__main__':
